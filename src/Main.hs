ab0fe15e92f486933f05f37d925fcd6d9640a413
azur <natapat.samutpong@gmail.com>
2024-05-24 00:18:51 +0700

    Read from file

--- src/Main.hs
+++ src/Main.hs
@@ -4,19 +4,22 @@ import Syntax
 import Parse
 import Infer
 
+import System.Environment
+
 main :: IO ()
 main = do
-
-  let test = "(\\f -> f 1)(\\x -> x)"
-
-  case runParser test of
-      Left err   -> print err
-      Right pexp -> do
-        putStrLn $ "Parsed: " ++ fmtExpr pexp
-        putStrLn $ "Inferring: " ++ fmtExpr pexp
-        case runInfer $ infer [] pexp of
+  args <- getArgs
+  case args of
+    [file] -> do
+      src <- readFile file
+      case runParser src of
+        Left err   -> print err
+        Right pexp -> do
+          putStrLn $ "Parsed       : " ++ fmtExpr pexp
+          case runInfer $ infer [] pexp of
             Left err -> putStrLn err
             Right (e, t, s)
-                -> putStrLn ("Inferred     : " ++ fmtExprT e)
-                >> putStrLn ("Expr Type    : " ++ fmtType t)
-                >> putStrLn ("Substitution : " ++ show s)
+              -> putStrLn ("Inferred     : " ++ fmtExprT e)
+              >> putStrLn ("Expr Type    : " ++ fmtType t)
+              >> putStrLn ("Substitution : " ++ show s)
+    _ -> putStrLn "Invalid arguments. Usage: cascadia <file>"
